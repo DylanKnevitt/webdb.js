59846f323720b6af2f4f77de75abaa8c5bfd19e5
Dylan Knevitt <dylan.knevitt@gmail.com>
2014-01-07 11:55:52 +0200

    Removed dbconnect.js and replaced it with dbwrapper.js

---
+++ dbwrapper.js
@@ -0,0 +1,13 @@
+function runQuery(query, driver) {
+    switch (driver) {
+    case "sqlite3":
+        var db = require('./sqlite');
+        return db.runQuery(query);
+       
+    default:
+        break;
+    }
+
+}
+
+console.log(runQuery("Select * from sqlite_master", "sqlite3"));
