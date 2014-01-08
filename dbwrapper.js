c5af76b5a1be60e9c0fa6a3cf082081aa48ef9b7
Dylan Knevitt <dylan.knevitt@gmail.com>
2014-01-08 11:51:04 +0200

    Updated Formatting

--- dbwrapper.js
+++ dbwrapper.js
@@ -1,13 +1,15 @@
 function runQuery(query, driver) {
-    switch (driver) {
+  switch (driver) {
     case "sqlite3":
-        var db = require('./sqlite');
-        return db.runQuery(query);
-       
+      var db = require('./sqlite');
+      return db.runQuery(query);
+    case "mongo":
+      break;
+    case "TSQL":
+      break;
     default:
-        break;
-    }
-
+      break;
+  }
 }
 
 //console.log(runQuery("Select * from sqlite_master", "sqlite3"));
