8762df3e9f07017ffaa0bf744d982b0302720a6c
Dylan Knevitt <dylan.knevitt@gmail.com>
2014-01-10 12:52:03 +0200

    Modified the server.js to server a html page.

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require('express');
-var app = express();
- 
-app.listen(3000);
 
-app.get('/', function(request, response) {
-   response.send("This would be some HTML");
+var http = require('http');
+var path = require('path');
+var app = express();
+
+app.configure(function() {
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.bodyParser());
+    app.use(express.logger("short"));
+});
+ 
+
+
+
+app.get('/', function (req, res) {
+    res.sendfile('./index.html');
 });
 
+app.listen(3000);
