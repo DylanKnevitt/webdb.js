4f19d8dc74c3f59652ad271b3b5dd4da037235c9
Dylan Knevitt <dylan.knevitt@gmail.com>
2014-01-10 12:54:00 +0200

    Added some of the old code

---
+++ public/javascript/sqlParser.js
@@ -0,0 +1,51 @@
+/*
+	Created By: Divan Visagie
+
+	This is just some skeleton code and the structure of this module could change completely,
+	so far all it needs to do is convert some text in codemirror into some usable commands
+*/
+
+if ( !window.hasOwnProperty('define') ){
+
+	window.define = function( name , deps , func ){
+
+		if ( !func && typeof deps === 'function' ){
+			func = deps;
+		}
+
+		window[ name ] = func();
+	};
+}
+
+define( 'sqlParser' , function(){
+
+	/*
+		This should determine whether or not a sql string is valid
+	*/
+	function validate(){
+
+		//TODO: make this work
+
+
+	}
+
+	/*
+		Takes in a bulk string of sql statements and returns a trimmed array
+	*/
+	return {
+		getLines : function ( sqlString ) {
+		  var cleanString = sqlString.replace(/:;:/g, ':semicolon:');
+			var lines = cleanString.split( ';' );
+		  lines.forEach(function(element,index,array) {
+		    lines[index] = element.replace(/:semicolon:/g, ';');
+		  });
+
+			for ( var i in lines ){
+
+				lines[i] = lines[i].trim();
+			}
+
+			return lines;
+		}
+	};
+} );
